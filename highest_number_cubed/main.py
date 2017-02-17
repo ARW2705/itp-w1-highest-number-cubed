e913b1fd3e6b4732e54e3e57bd4ea1dda5c8a534
Andrew Wanex <awanex2705@gmail.com>
2017-02-17 01:47:18 +0000

    Update to pass additional tests

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -4,7 +4,7 @@
 def highest_number_cubed(limit):
     test = 1
     while True:
-        if test ** 3 < limit:
+        if test ** 3 <= limit:
             test += 1
         else:
             return (test - 1)  
